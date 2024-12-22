4d249b17be5aef650a4bd5fc9dc0a12f232dac57
Mahendra Nakar <mahin.nakar@gmail.com>
2024-12-22 08:04:07 +0530

    bugs: fixed minir changes

--- packages/plugins/@nocobase/plugin-whatsapp/src/client/schemas/index.ts
+++ packages/plugins/@nocobase/plugin-whatsapp/src/client/schemas/index.ts
@@ -31,62 +31,59 @@ export function getWhatsAppSchema({ dataSource = 'main', collection }) {
     //   collection,
     //   action: 'list',
     // },
-   // 'x-settings': 'blockSettings:table',
-    // 'x-component': 'CardItem',
-    //'x-toolbar': 'BlockSchemaToolbar',
-
+    
+    'x-component': 'CardItem',
+    'x-toolbar': 'BlockSchemaToolbar',
+    'x-initializer': 'table:configureItemActions', 
     'x-settings': 'blockSettings:table',    
     'x-component': 'CardItem',
     'x-decorator': 'TableBlockProvider',
-    'x-use-decorator-props': 'useTableBlockDecoratorProps',
     'x-decorator-props': {
-      collection: 'users',
-      dataSource: 'main',
+      collection,
       action: 'list',
-      params: {
-        pageSize: 20,
-      },
-      rowKey: 'id',
       showIndex: true,
       dragSort: false,
     },
 
-
-
-
     properties: {
-       whatsapp: {
-         type: 'void',
-         //'x-component': "PluginWhatsappSession",
-         'x-component': "TableV2.Column",
-         //'x-use-component-props': 'useWhatsAppProps',
-         'x-initializer': 'table:configureActions',
-       },
-      actions: {
-                // type: 'void',
-                // title: '{{ t("Actions") }}',
-                // 'x-action-column': 'actions',
-                // 'x-decorator': 'TableV2.Column.ActionBar',
-                // 'x-component': 'TableV2.Column',
-                // 'x-designer': 'TableV2.ActionColumnDesigner',
-                // 'x-initializer': 'table:configureItemActions',
-                // 'x-component-props': {
-                //   width: 400,
-                // },
-      },
+      // whatsapp: {
+      //   type: 'void',
+      //   'x-component': "PluginWhatsappSession",
+      //   'x-use-component-props': 'useWhatsAppProps',
+      //   'x-initializer': 'table:configureActions',
+      // }
+
       table: {
-      // type: 'array',
-      // 'x-component': 'TableV2',
-      // 'x-use-component-props': 'useTableBlockProps',
-      // 'x-initializer': 'table:configureActions',
-      // 'x-designer': 'TableV2.ActionColumnDesigner',                                                                  
-      // 'x-component-props': {
-      //   rowKey: 'id',
-      //   rowSelection: {
-      //     type: 'checkbox',
-      //   },
-      //   useDataSource: '{{ cm.useDataSourceFromRAC }}'
-      // },
+      type: 'array',
+      'x-component': 'TableV2',
+      'x-use-component-props': 'useTableBlockProps',
+      'x-initializer': 'table:configureColumns',
+      'x-designer': 'TableV2.ActionColumnDesigner', 
+      'x-component-props': {
+        rowKey: 'id',
+        rowSelection: {
+          type: 'checkbox',
+        },
+        //useDataSource: '{{ cm.useDataSourceFromRAC }}',
+       // 'x-initializer': 'table:configureItemActions',                                                                 
+      
+      },
+      properties: {
+        
+        actions: {
+                type: 'void',
+                title: '{{ t("Actions") }}',
+                'x-action-column': 'actions',
+                'x-decorator': 'TableV2.Column.ActionBar',
+                'x-component': 'TableV2.Column',
+                'x-designer': 'TableV2.ActionColumnDesigner',
+                'x-component-props': {
+                 // width: 400,
+                },
+      },  
+      }
+
+      
     },
     }
   }
